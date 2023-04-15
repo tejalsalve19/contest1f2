6200b0d5ab4e518d8818ce78a81f71a723d29c53
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-04-16 03:19:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  console.log(arr);
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  console.log(arr);
 }
 
 function addData() {
   //Write your code here, just console.log
+  // arr.push(4,"susan","20","intern");
+  arr.push({ id: 4, name: "susan", age: "20", profession: "intern" });
+  console.log(arr);
 }
 
 function removeAdmin() {
@@ -24,4 +29,11 @@ function removeAdmin() {
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2=[
+    { id: 10, name: "jerry", age: "8", profession: "teacher" },
+  { id: 20, name: "tom", age: "12", profession: "programmer" },
+  { id: 30, name: "shinchan", age: "5", profession: "student" },
+  ];
+  let answer=arr.concat(arr2);
+  console.log(answer);
 }
